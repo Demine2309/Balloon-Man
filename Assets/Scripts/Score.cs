084c0842cc2a164e0aa5478c2b241e5298118025
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-05-22 23:30:35 +0700

    Update UI
    22/5/2024

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private float score;
+    public Text scoreText;
+    private int score = 0;
+
+    private float playetHeight = 1f;
 
     [SerializeField] private PlayerMovement player;
 
+    private void Start()
+    {
+
+    }
+
     private void Update()
     {
-        
+        scoreText.text = score.ToString();
+
+        float currentHeight = player.transform.position.y;
+
+        currentHeight = Mathf.Max(0f, currentHeight);
+
+
     }
 }
