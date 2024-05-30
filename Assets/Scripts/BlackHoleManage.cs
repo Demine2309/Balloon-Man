79addd3d75429eacd385d390e3e3a53dd8cc0484
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-05-30 22:04:28 +0700

    Update raycast
    30/5/2024

--- Assets/Scripts/BlackHoleManage.cs
+++ Assets/Scripts/BlackHoleManage.cs
@@ -6,16 +6,27 @@ public class BlackHoleManage : MonoBehaviour
 {
     [SerializeField] private float dragForce;
 
-    [SerializeField] private Transform point1;
-    [SerializeField] private Transform point2;
-    [SerializeField] private Transform point3;
+    [SerializeField] private List<Transform> points;
 
-    private void OnDrawGizmos()
+
+    private void Update()
     {
-        Gizmos.color = Color.green;
+        for (int i = 1; i < points.Count; i++)
+        {
+            int layerMask = LayerMask.GetMask("Player");
 
-        Gizmos.DrawLine(point1.position, point2.position);
-        Gizmos.DrawLine(point2.position, point3.position);
-        Gizmos.DrawLine(point3.position, point1.position);
+            Vector2 direction = points[i].position - points[0].position;
+
+            RaycastHit2D hit = Physics2D.Raycast(points[0].position, direction, 10f, layerMask);
+
+            Color rayColor = Color.green;
+
+            if (hit.collider != null)
+                rayColor = Color.red;
+            else
+                rayColor = Color.green;
+
+            Debug.DrawRay(points[0].position, direction * 10f, rayColor);
+        }
     }
 }
